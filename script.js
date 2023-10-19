0c2785db6dc255dd7201f5010173424923fafbfd
Shruti Garg <138093537+Gargshruti19@users.noreply.github.com>
2023-10-19 12:17:27 +0530

    All files added.

---
+++ script.js
@@ -0,0 +1,25 @@
+const apiUrl = "https://api.quotable.io/random";
+const newquote = document.querySelector("#new");
+const tweet = document.querySelector("#tweet");
+async function getQuote(url) {
+	const response = await fetch(url);
+	var data = await response.json(url);
+	console.log(data);
+	document.querySelector("blockquote").innerHTML = data.content;
+	document.querySelector("span").innerHTML = data.author;
+}
+getQuote(apiUrl);
+newquote.addEventListener("click", function () {
+	getQuote(apiUrl);
+});
+
+tweet.addEventListener("click", function () {
+	window.open(
+		"https://twitter.com/intent/tweet?text=" +
+			document.querySelector("blockquote").innerHTML +
+			"---- by " +
+			document.querySelector("span").innerHTML,
+		"Tweet window",
+		"width = 600, height=300"
+	);
+});
